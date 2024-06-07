1f68d1de6f933bdb0aca7faa2a875b1d6a1c9901
cmirah <158570014+cmirah@users.noreply.github.com>
2024-06-07 21:45:42 +0800

    Update Web_Based.py

--- Web_Based.py
+++ Web_Based.py
@@ -40,8 +40,7 @@ def run():
         ### More information
         - Data from [MoH-Malaysia](https://github.com/MoH-Malaysia/covid19-public.git)
         - Jump into my [amirah's app](https://share.streamlit.io/)
-        - Ask a question in Streamlit's [community
-          forums](https://discuss.streamlit.io)
+        - Web-Based apps solely developed by Amirah Alysha bt Azman in completion of Final Year Project II
     """
     )
 
