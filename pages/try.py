40f168125129f931034f7cabf8767cfc118f7174
cmirah <158570014+cmirah@users.noreply.github.com>
2024-06-09 22:10:43 +0800

    Update try.py

--- pages/try.py
+++ pages/try.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
@@ -97,6 +98,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
 
