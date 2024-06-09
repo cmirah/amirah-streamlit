da91e1ca5e105cef1b44b3d251aa897f385409e2
cmirah <158570014+cmirah@users.noreply.github.com>
2024-06-09 20:16:18 +0800

    Update try2.py

--- pages/try2.py
+++ pages/try2.py
@@ -3,10 +3,9 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-from keras.models import Sequential
-from keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 import matplotlib.pyplot as plt
-import tensorflow as tf
 
 # Load data function
 @st.cache
@@ -16,42 +15,38 @@ def load_data(file_path):
 
 # Preprocess data function
 def preprocess_data(df):
-    train_data = df.loc[0:250]
-    valid_data = df.loc[251:len(df)]
+    train_data = df.iloc[:250]
+    valid_data = df.iloc[250:]
     
-    x_train = train_data[['susceptible', 'infected', 'recovered', 'fatal']]
-    y_train = train_data[['infected']]
-    x_valid = valid_data[['susceptible', 'infected', 'recovered', 'fatal']]
-    y_valid = valid_data[['infected']]
+    x_train = train_data[['susceptible', 'infected', 'recovered', 'fatal']].values
+    y_train = train_data['infected'].values.reshape(-1, 1)
+    x_valid = valid_data[['susceptible', 'infected', 'recovered', 'fatal']].values
+    y_valid = valid_data['infected'].values.reshape(-1, 1)
     
     scaler_x = MinMaxScaler()
     scaler_y = MinMaxScaler()
     
-    x_train_scale = scaler_x.fit_transform(x_train)
-    x_valid_scale = scaler_x.transform(x_valid)
+    x_train = scaler_x.fit_transform(x_train)
+    x_valid = scaler_x.transform(x_valid)
+    y_train = scaler_y.fit_transform(y_train)
     
-    y_train_scale = scaler_y.fit_transform(np.reshape(y_train.values, (-1, 1)))
-    y_valid_scale = scaler_y.transform(np.reshape(y_valid.values, (-1, 1)))
-    
-    return x_train_scale, y_train_scale, x_valid_scale, y_valid_scale, scaler_y, y_valid.values
+    return x_train, y_train, x_valid, y_valid, scaler_y
 
 # Build and train model function
 def build_and_train_model(x_train, y_train, epochs=100):
-    with tf.name_scope('Model'):
-        model = Sequential()
-        model.add(Dense(4, input_dim=4, kernel_initializer='normal', activation='relu'))
-        model.add(Dense(70, activation='relu'))
-        model.add(Dense(1, activation='linear'))
-        
-        model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae'])
-        
-        history = model.fit(x_train, y_train, epochs=epochs, batch_size=150, verbose=0, validation_split=0.2)
+    model = Sequential([
+        Dense(70, input_shape=(x_train.shape[1],), activation='relu'),
+        Dense(1, activation='linear')
+    ])
+    
+    model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae'])
+    
+    history = model.fit(x_train, y_train, epochs=epochs, batch_size=32, validation_split=0.2, verbose=1)
     
     return model, history
 
 # Evaluate model function
 def evaluate_model(model, x_valid, y_valid, scaler_y):
-    x_valid = np.reshape(x_valid, (x_valid.shape[0], -1))
     y_pred_scale = model.predict(x_valid)
     y_pred = scaler_y.inverse_transform(y_pred_scale)
     
@@ -72,14 +67,13 @@ def main():
         df = load_data(file_path)
         st.write(df.tail())
         
-        x_train, y_train, x_valid, y_valid, scaler_y, y_valid_raw = preprocess_data(df)
+        x_train, y_train, x_valid, y_valid, scaler_y = preprocess_data(df)
         
         st.session_state['x_train'] = x_train
         st.session_state['y_train'] = y_train
         st.session_state['x_valid'] = x_valid
         st.session_state['y_valid'] = y_valid
         st.session_state['scaler_y'] = scaler_y
-        st.session_state['y_valid_raw'] = y_valid_raw
         
         st.write("Data Preprocessed Successfully")
     
@@ -120,7 +114,7 @@ def main():
             st.write(f"MAPE: {MAPE:.4f}")
             
             # Plot actual vs predicted
-            df_result = pd.DataFrame({'Actual': st.session_state['y_valid_raw'].flatten(), 'Predicted': y_pred.flatten()})
+            df_result = pd.DataFrame({'Actual': st.session_state['y_valid'].flatten(), 'Predicted': y_pred.flatten()})
             st.write(df_result)
             
             fig, ax = plt.subplots(figsize=(20, 10))
